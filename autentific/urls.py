91937834b0fd6f2595531e975cf28b1aef57ff13
bahodirov07uz <muhammadyahyobahodirov376@gmail.com>
2025-06-21 16:33:06 +0500

    Update urls.py

--- autentific/urls.py
+++ autentific/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import login_page,register_view
+from .views import login_page,register_view,custom_logout
 app_name = 'autentific'
 urlpatterns = [
     path('',login_page, name='login_page'),
-    path('secret-register/',register_view,name='register_page')
+    path('secret-register/',register_view,name='register_page'),
+    path('secret-logout/', custom_logout, name='logout'),
 ]   
 
